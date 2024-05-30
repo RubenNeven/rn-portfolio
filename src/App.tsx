8abfd2024a397e60ed5e3cc0077f37f957fe07e6
rneven <ruben.neven@student.kdg.be>
2024-05-30 20:44:47 +0200

    refactored App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Header from "./Header";
 import Hero from "./Hero";
-import Experience from "./Experience.tsx";
 
 
 function App(){
   return <>
     <Header />
     <Hero />
-    <Experience/>
 </>
 }
 
