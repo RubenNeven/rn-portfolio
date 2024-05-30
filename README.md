be2a98f2e2c5f2565b2f67e480c5484c321a7255
rneven <ruben.neven@student.kdg.be>
2024-05-30 14:13:07 +0200

    hero media-queries

--- README.md
+++ README.md
@@ -28,3 +28,6 @@ export default {
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+
+- Font sizes (px)
+  10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98
--- src/Hero.tsx
+++ src/Hero.tsx
@@ -3,19 +3,24 @@ import { Cursor, useTypewriter } from 'react-simple-typewriter';
 export default function Hero(){
 
    const [skills] = useTypewriter({
-      words: ['back-end', 'front-end', 'cloud'],
+      words: ['Back-end', 'Front-end', 'Cloud'],
       loop: true
    });
    return (
       <section id="hero">
          <div className="hero-text">
             <h1>Ruben Neven</h1>
-            <p >I'm <span className="skill">{skills}</span><Cursor/>developer </p>
+            <p ><span className="skill">{skills}</span><Cursor/>developer </p>
             <div className="hero__cta">
                <a href="#">
-                  Let's connect &rarr; 
+                  Let's connect &rarr;
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
                </a>
-                  
             </div>
             
          </div>   
